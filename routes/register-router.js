c721c4f953ff40b514e272991981582dfd7603da
Talita-8 <tlthmf1@outlook.com>
2021-04-12 15:10:52 -0300

    Controllers Functions

--- routes/register-router.js
+++ routes/register-router.js
@@ -4,6 +4,6 @@ const  Register = require('../controllers/register-controller')
 const router = Router()
 
 router.get('/', Register.getAll)
-router.post('/', Register.register )
+router.post('/', Register.register)
 
 module.exports = router
