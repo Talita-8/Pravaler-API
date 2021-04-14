8f33eee8ae14028c1e037814ce2f350e0171409b
Kaueny Alves <kaueny@MacBook-Pro-de-Kaueny.local>
2021-04-14 20:31:27 -0300

    login

--- routes/schools-router 2.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const  School = require('../controllers/schools-controller')
-
-const router = Router()
-
-router.get('/', School.getAll)
-router.post('/', School.register)
-
-module.exports = router
